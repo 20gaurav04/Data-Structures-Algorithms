b278fdd70337867a4ac51df4358f762d32c7f1c2
20gaurav04 <btechug2026@gmail.com>
2024-11-24 13:50:51 +0530

    recursion

--- src/Recursion/find.java
+++ src/Recursion/find.java
@@ -1,4 +1,5 @@
 package Recursion;
+import java.util.*;
 
 public class find {
     static boolean find(int[] arr , int index , int target)
@@ -9,11 +10,40 @@ public class find {
 
     }
 
+
+    static List<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr , int index , int target) {
+        if (index == arr.length) {
+            return;
+        } else if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, index + 1, target);
+    }
+    static ArrayList findAll(int[] arr , int index , int target , ArrayList<Integer>list)
+    {
+        if(index==arr.length)
+        {
+            return list;
+        }
+        if(arr[index]==target)
+        {
+            list.add(index);
+        }
+
+        return findAll(arr,index+1 , target , list);
+    }
+
     public static void main(String[] args) {
-        int [] arr =  { 1,2,3};
+        int [] arr =  { 1,2,3,3};
         int target = 3;
-        boolean res = find(arr,0,target);
-        System.out.println(res);
+//        boolean res = find(arr,0,target);
+//        System.out.println(res);
+//        findAllIndex(arr,0,target);
+//        System.out.println(list);
+        List<Integer> resList = findAll(arr,0,target,new ArrayList<>());
+        System.out.println(resList);
 
     }
+
 }
