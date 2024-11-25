4e44861372b5640f97902b9a9ed9690ebca0bda6
20gaurav04 <btechug2026@gmail.com>
2024-11-25 22:46:55 +0530

    recursion

--- src/Recursion/Pattern.java
+++ src/Recursion/Pattern.java
@@ -3,8 +3,10 @@ package Recursion;
 public class Pattern {
     public static void main(String[] args) {
 
-        pattern(4,0);
+//        pattern(4,0);
+        pattern2(4,0);
     }
+
     static void pattern(int row , int col)
     {
         if(row==0)
@@ -20,6 +22,25 @@ public class Pattern {
         {
             System.out.println();
             pattern(row-1,0);
+        }
+    }
+
+    static void pattern2(int row , int col)
+    {
+        if(row==0)
+        {
+            return;
+        }
+        if(col<row)
+        {
+
+            pattern2(row , col+1);
+            System.out.print(" * ");
+        }
+        else
+        {
+            pattern2(row-1,0);
+            System.out.println();
         }
     }
 }
