0ff375261de98ca08c90baf5d9e4da4de1b9f252
20gaurav04 <btechug2026@gmail.com>
2025-01-11 21:24:11 +0530

    recursion

---
+++ src/Strings/RemovingACharacter.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class RemovingACharacter {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+//        string builder is mutable and unlike strings it modifies the original object itself
+        sb.deleteCharAt(2); // Deletes the character at index 2
+        sb.append("hello");
+        sb.insert(0,"y");
+        System.out.println(sb);
+        String str = "Greatest Of All Time ";
+        for (char ch : str.toCharArray()) {
+            System.out.print(ch);
+        }
+
+    }
+}
