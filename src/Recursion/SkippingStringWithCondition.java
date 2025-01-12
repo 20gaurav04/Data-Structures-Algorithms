44bcbec7ea25d90b3d16e5befd7fffbccb08636e
20gaurav04 <btechug2026@gmail.com>
2025-01-12 16:19:03 +0530

    Strings

---
+++ src/Recursion/SkippingStringWithCondition.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class SkippingStringWithCondition {
+    public static void main(String[] args) {
+        String s = "redappe";
+        String res = skip(s);
+        System.out.println(res);
+
+    }
+    static String skip(String s)
+    {
+        if(s.isEmpty())
+        {
+            return "" ;
+        }
+        if(s.startsWith("app") && !s.startsWith("apple"))
+//        it will skip app only when it is not apple
+        {
+            return skip(s.substring(3));
+        }
+        else
+        {
+            return s.charAt(0)+skip(s.substring(1));
+        }
+    }
+}
