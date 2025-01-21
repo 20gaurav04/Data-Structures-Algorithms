791294dfa6347058cd3c063c4be25df316ca8a30
20gaurav04 <btechug2026@gmail.com>
2025-01-21 22:39:15 +0530

    Strings

--- src/Recursion/Permutation.java
+++ src/Recursion/Permutation.java
@@ -1,11 +1,15 @@
 package Recursion;
+import java.util.ArrayList;
 
 public class Permutation {
     public static void main(String[] args) {
         String p = "";
         String up = "abc";
-        permutation(p,up);
+//        permutation(p,up);
+        ArrayList<String> ans = new ArrayList<>(perms(p,up));
+        System.out.println(ans);
     }
+
     static void permutation(String p , String up)
     {
         if(up.isEmpty())
@@ -20,5 +24,25 @@ public class Permutation {
             String s = p.substring(i,p.length());
             permutation(f+ch+s,up.substring(1));
         }
+    }
+
+    static ArrayList<String> perms(String p , String up)
+    {
+        if(up.isEmpty())
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        ArrayList<String> ans = new ArrayList<>();
+        for(int i = 0 ; i<=p.length();i++)
+        {
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            ans.addAll(perms(f+ch+s,up.substring(1)));
+        }
+        return ans;
     }
 }
