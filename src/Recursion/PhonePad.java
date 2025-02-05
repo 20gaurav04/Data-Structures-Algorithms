5722f3c284c98a0f50d51e40e0abbc5eba4f3a16
20gaurav04 <btechug2026@gmail.com>
2025-02-05 21:53:11 +0530

    Strings

--- src/Recursion/PhonePad.java
+++ src/Recursion/PhonePad.java
@@ -1,21 +1,31 @@
 package Recursion;
+import java.util.ArrayList;
+import java.util.List;
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 public class PhonePad {
     public static void main(String[] args) {
-        pad("","12");
+        List<String> res = pad("","12");
+        System.out.println(res);
+
     }
-    static void pad(String p , String up)
+    static List<String> pad(String p , String up)
     {
-        if(up.isEmpty())
-        {
-            System.out.println(p);
-            return;
+        List<String> res = new ArrayList<>();
+        if (up.isEmpty()) {
+            res.add(p);
+            return res;
         }
+
         int digit = up.charAt(0) - '0';
-        for(int i = (digit-1)*3 ; i<digit*3;i++)
-        {
-            char ch = (char)('a' + i);
-            pad(p+ch,up.substring(1));
+        int start = (digit - 1) * 3;
+        int end = digit * 3;
+
+        for (int i = start; i < end; i++) {
+            char ch = (char) ('a' + i);
+            res.addAll(pad(p + ch, up.substring(1)));
         }
+
+        return res;
     }
 }
