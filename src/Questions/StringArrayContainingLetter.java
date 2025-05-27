f9ec10622046600229ccc7a28751d09e2ffbd36f
20gaurav04 <btechug2026@gmail.com>
2025-05-27 20:00:12 +0530

    HalfSubArraySum

---
+++ src/Questions/StringArrayContainingLetter.java
@@ -0,0 +1,21 @@
+package Questions;
+import java.util.*;
+//https://leetcode.com/problems/find-words-containing-character/?envType=daily-question&envId=2025-05-27
+public class StringArrayContainingLetter {
+    public static void main(String[] args) {
+        String[] arr = {"hello","lmao","crazy","oppenheimer"};
+        char x = 'a';
+        System.out.println(findWordsContaining(arr,x));
+    }
+    static List<Integer> findWordsContaining(String[] arr , char x)
+    {
+        List<Integer> lst = new ArrayList<>();
+        for(int i = 0 ; i<arr.length;i++)
+        {
+            if(arr[i].contains(String.valueOf(x))){
+                lst.add(i);
+            }
+        }
+        return lst;
+    }
+}
