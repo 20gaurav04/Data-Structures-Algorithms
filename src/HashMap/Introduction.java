9955a63cf00e5330085a848e3befe627b753f0a4
20gaurav04 <btechug2026@gmail.com>
2025-08-01 07:05:35 +0530

    HashMap Introduction

---
+++ src/HashMap/Introduction.java
@@ -0,0 +1,19 @@
+package HashMap;
+import java.util.*;
+public class Introduction {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map = new HashMap<>();
+//        Stores in the form of key-value pairs
+
+        map.put(1,"Gaurav");
+        map.put(2,"Saurav");
+        map.put(3,"Jiya");
+
+        System.out.println(map);
+        System.out.println(map.containsKey(2));
+        System.out.println(map.containsValue("Om"));
+        System.out.println(map.get(1));
+        boolean removed = map.remove(1,"Mohena");
+        System.out.println("remove" + " " + removed);
+    }
+}
